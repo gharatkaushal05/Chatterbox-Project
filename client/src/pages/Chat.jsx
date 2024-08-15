48b123cddb98389d3e30c7ebfb2afb56802a434e
gharatkaushal05 <gharatkaushal05@gmail.com>
2024-08-15 21:50:55 +0530

    Initial commit

---
+++ client/src/pages/Chat.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AppLayout from "../components/layout/AppLayout"
+
+function Chat() {
+  return (
+    <div>
+      Chat
+    </div>
+  )
+}
+
+export default AppLayout()(Chat)
